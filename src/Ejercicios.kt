af12dd91dc229e8f919a5b89e0f61d9a4ad275da
Diego <lop23747@uvg.edu.gt>
2024-07-17 20:27:10 -0600

    first commit

---
+++ src/Ejercicios.kt
@@ -0,0 +1,20 @@
+class Ejercicios {
+
+//    Escriba una función calcularPromedio que tome una lista de números y devuelva su
+//    promedio. Utilice la función de reducción para calcular la suma de los elementos.
+//    ● Cree una lista de enteros, luego use la función de filter para extraer números impares y
+//    almacenarlos en una nueva lista.
+//    ● Escriba una función compacta isPalindrome que tome una cadena como entrada y
+//    devuelva verdadero si es un palíndromo (lee lo mismo hacia atrás que hacia adelante), y
+//    falso de lo contrario.
+//    ● Cree una lista de cadenas que representen nombres. Use la función de map para agregar
+//    un saludo antes de cada nombre, como "¡Hola, Alicia!"
+//    ● Defina una función performOperation que tome dos enteros y una lambda como
+//    parámetros. La lambda debe representar una operación matemática (por ejemplo, suma,
+//    resta). La función debe devolver el resultado de la operación en los dos enteros.
+//    ● Defina dos clases
+//
+
+
+
+}
